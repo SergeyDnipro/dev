a29e274ba9c1709ad9be6498274928eebaa3da8d
sergey <bikeshop.dnipro@gmail.com>
2023-02-14 01:07:49 +0200

    14/03/2023 home commit

--- django_site/django_app/views/IndexView.py
+++ django_site/django_app/views/IndexView.py
@@ -10,15 +10,15 @@ class StartPage(TemplateView):
     template_name = 'django_app/index.html'
 
     def get_context_data(self, **kwargs):
-        result = Record.objects.filter(created=timezone.now() - timezone.timedelta(days=2))
+        result = Record.objects.filter(created__month=timezone.now().month)
         return {'items': [
             {
                 'user': items.holder,
                 'start_date': items.created,
-                'description' : items.description,
+                'description': items.description,
                 'status': items.status,
-                'user' : items.holder.username,
-                'user_id' : items.holder_id,
+                'user': items.holder.username,
+                'user_id': items.holder_id,
             }
             for items in result
         ]
